92ccc405e4b10caa48274376d0c670f447abeef8
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-14 22:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	for(let i=0;i<text.length;i++){
+      if(text.charAt(i)!=" "){
+          return text.charAt(i);
+      }
+  }
+  let ans = " ";
+  return ans;
+	
 }
 
 // Do not change the code below
